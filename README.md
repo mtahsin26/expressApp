67f494be0954ffdeed140858b4f942fe816d2ad2
Tahsin <mtahsin26@gmail.com>
2017-03-18 11:30:18 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+setting up basic node/express application.
+
---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.get("/", function(req, res){
+    res.send("Testing Express");
+    
+});
+
+//Starts server
+app.listen("3000", function(){
+
+    console.log("Running server");
+});
+
